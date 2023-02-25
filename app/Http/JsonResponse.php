b26485c8ac3aac14ed0a7a8ed356c57d48a2c53a
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-25 00:35:25 -0300

    feat: create JsonResponse class for handling with http responses

---
+++ app/Http/JsonResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http;
+
+use RuntimeException;
+
+class JsonResponse
+{
+    public static function create(array $data, int $statusCode = 200): void
+    {
+        $json = json_encode($data);
+
+        if ($json === false) {
+            throw new RuntimeException('Error encoding JSON data');
+        }
+
+        $contentLength = strlen($json);
+
+        http_response_code($statusCode);
+        header('Content-Type: application/json');
+        header("Content-Length: {$contentLength}");
+
+        echo $json;
+
+        exit();
+    }
+}
