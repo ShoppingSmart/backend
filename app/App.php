9e54ef61c20023d0a6468896ce09d449f21405fd
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-25 17:53:30 -0300

    feat: create app container class

---
+++ app/App.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class App
+{
+    /*
+    * Creates a new instance of the App class.
+    *
+    * @param string $param1 The first parameter of the constructor.
+    * @param int $param2 The second parameter of the constructor.
+    * @throws InvalidArgumentException If the $param1 argument is not a string.
+    */
+    public function __construct(...$args)
+    {
+        // Constructor logic goes here
+    }
+}
