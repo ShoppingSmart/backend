ff8a87248f6dd5254377b6aa3813a3fd8f7bcfe7
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-26 20:17:54 -0300

    feat: fix env read method

--- framework/Config/Env.php
+++ framework/Config/Env.php
@@ -16,10 +16,12 @@ class Env
 
     public function read($arquivo)
     {
+        $filename = root_path($arquivo);
+
         // Verifica se o arquivo existe
-        if (file_exists($arquivo)) {
+        if (file_exists($filename)) {
             // Lê o arquivo INI e retorna um array associativo
-            return parse_ini_file($arquivo);
+            return parse_ini_file($filename);
         } else {
             // Retorna um array vazio em caso de falha na leitura do arquivo
             return [];
