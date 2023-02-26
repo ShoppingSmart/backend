c7df707e4c53074411bc0ae4818b6fcefcba7c47
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-26 20:15:52 -0300

    feat: product controller index implementation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Request;
+use App\Models\Product;
 use Framework\Http\Controllers\Index;
 
 class ProductController extends Controller implements Index
 {
     public function index(Request $request): array
     {
-        return json_decode(file_get_contents(
-            database_path('seeders/products.json')
-        ));
+        return Product::newModelInstance()->all();
     }
 }
