66f21c733ccd982aa5ee4eefd0971a7b5a6b9de7
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-27 10:20:53 -0300

    feat: store order and order products implementation

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Request;
 use App\Models\Order;
+use App\Models\OrderProduct;
+use Exception;
 
 class OrderController extends Controller
 {
@@ -15,5 +17,35 @@ class OrderController extends Controller
     public function index(Request $request): array
     {
         return Order::newModelInstance()->all();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return App\Http\Request
+     */
+    public function store(Request $request): array
+    {
+        $payload = $request->all();
+
+        if (empty((array) $payload)) {
+            throw new Exception('Please add at least one item to your cart');
+        }
+
+        $orderId = Order::newModelInstance()->create([
+            'created_at' => date("Y/m/d H:i:s")
+        ]);
+
+        foreach ($payload as $orderItem) {
+            OrderProduct::newModelInstance()->create([
+                'product_id' => $orderItem['id'],
+                'quantity' => $orderItem['quantity'],
+                'order_id' => $orderId
+            ]);
+        }
+
+        return [
+            'id' => $orderId
+        ];
     }
 }
