19923e40be85c0078d5900752e675719d52b32a8
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-27 09:44:58 -0300

    feat: order index http implementation

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Request;
+use App\Models\Order;
+
 class OrderController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return App\Http\Request
+     */
+    public function index(Request $request): array
+    {
+        return Order::newModelInstance()->all();
+    }
 }
